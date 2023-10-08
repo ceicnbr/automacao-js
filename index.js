da34fa963449384bb665e24e39e1805e2d7ce3fd
ceicnbr <ceicnbrl@gmail.com>
2023-10-08 09:49:30 -0400

    implementação de library para pedir usuario e pass

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const prompt = require('prompt-sync')({sigint:true});
+
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -8,8 +10,10 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('http://10.56.160.77/acesso/index2.php');
 // fazer login na pagina
-    await page.type('#txUsuario','0908729478')
-    await page.type('#txSenha','0908729478')
+    usuario = prompt("digite o seu usuário: ");
+    senha = prompt("Digite sua senha: ")
+    await page.type('#txUsuario',usuario)
+    await page.type('#txSenha',senha)
     await page.click('#entrar');
 //pular alert
     page.on('dialog', async dialog => {
@@ -17,8 +21,9 @@ const puppeteer = require('puppeteer');
      });
     //aguardar a pagina carregar para ir direto pra pagina de arranchamento 
     await page.waitForNavigation();
-
     await page.goto('http://10.56.160.77/acesso/usuario_arranchar_novo.php')
+    
+    
     // Seletor da classe CSS .arranchar para clicar 
     const classeCSS = '.arranchar';
     let elementos = await page.$$(classeCSS);
@@ -33,12 +38,14 @@ const puppeteer = require('puppeteer');
   
       // Atualize a lista de elementos, pois a página pode ter mudado
       elementos = await page.$$(classeCSS);
+
+      //caso o de cima não funcionar descomenta essa linha abaixo
       /* await page.waitForSelector(classeCSS); */
 
       // Avance para o próximo elemento
       index += 0;
     }
-    // se a contagem de classes .arranchar for igual a 0 fechar o navegador
+    // se a contagem de classes .arranchar for igual a zero fechar o navegador
     if (index == 0){
         await browser.close();
     }
